a8d3d704ac5a88ef192f9f35e5cd84eb2858a1c2
abdalrhman <abdalrhmanamr14@gmail.com>
2024-08-22 00:36:07 +0300

    draw polyLines

--- lib/features/google_map/logic/google_map_cubit.dart
+++ lib/features/google_map/logic/google_map_cubit.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps/core/utlis/location_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 part 'google_map_state.dart';
+
 
 class GoogleMapCubit extends Cubit<GoogleMapState> {
   GoogleMapCubit() : super(GoogleMapInitial());
@@ -14,6 +17,35 @@ class GoogleMapCubit extends Cubit<GoogleMapState> {
   GoogleMapController? mapController;
   Set<Marker> markers = {};
   Marker? searchedLocationMarker;
+  bool isSearched = false;
+  LatLng currentLocation = const LatLng(0, 0);
+  LatLng searchedLocation = const LatLng(0, 0);
+  Set<Polyline> polyLines = {};
+
+  PolylinePoints polylinePoints = PolylinePoints();
+
+  void drawRoute()async {
+    await polylinePoints.getRouteBetweenCoordinates(
+      googleApiKey: dotenv.env['google_api_key']!,
+      request: PolylineRequest(
+        origin: PointLatLng(currentLocation.latitude, currentLocation.longitude),
+        destination: PointLatLng(searchedLocation.latitude, searchedLocation.longitude),
+        mode: TravelMode.driving,
+
+      ),
+
+    ).then((value) {
+      polyLines.add(Polyline(
+        polylineId: const PolylineId('route'),
+        color: Colors.blue,
+        points: value.points.map((e) => LatLng(e.latitude, e.longitude)).toList(),
+      ));
+    });
+    polylinePoints.decodePolyline("_p~iF~ps|U_ulLnnqC_mqNvxq`@");
+    isSearched = false;
+    emit(UpdateCurrentLocation());
+  }
+
 
   TextEditingController googleMapAutoCompleteController =
       TextEditingController();
@@ -27,6 +59,7 @@ class GoogleMapCubit extends Cubit<GoogleMapState> {
         infoWindow: const InfoWindow(title: 'Your Location'),
       ));
       LatLng latLng = LatLng(locationData.latitude!, locationData.longitude!);
+      currentLocation = latLng;
       animateCameraPosition(latLng);
       emit(UpdateCurrentLocation());
     } on LocationServiceException catch (e) {
@@ -38,12 +71,15 @@ class GoogleMapCubit extends Cubit<GoogleMapState> {
 
   Future<void> goToSearchedLocation(LatLng latLng) async {
     markers.remove(searchedLocationMarker);
+    polyLines.clear();
     searchedLocationMarker = Marker(
       markerId: const MarkerId('searched_location'),
       position: latLng,
     );
     markers.add(searchedLocationMarker!);
+    searchedLocation = latLng;
     animateCameraPosition(latLng);
+    isSearched = true;
     emit(UpdateCurrentLocation());
   }
 
